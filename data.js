65e8c6af222e4537826f5a36437bf2190d8491e8
Edward Pak <pakedward@gmail.com>
2026-05-28 16:41:01 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <input type="password" id="pass" placeholder="Password"><button onclick="load()">Enter</button>
-    <div id="view"></div>
-    <script src="data.js"></script>
-    <script>
-        function load() {
-            if(document.getElementById('pass').value !== "YOUR_SECRET_PASSWORD") return alert("Denied");
-            const view = document.getElementById('view');
-            clientWorkspaceData.forEach(a => {
-                view.innerHTML += `<div><h3>[${a.type}] ${a.title.en}</h3><p>${a.payload}</p><hr></div>`;
-            });
-        }
-    </script>
-</body>
-</html>
+const clientWorkspaceData = [];
